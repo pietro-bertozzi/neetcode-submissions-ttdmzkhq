fd2e7f677a7e08d8b0e08ff7d9470b7ec2855b36
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2026-04-01 12:13:02 +0200

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int best = nums[0], sum = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            if (sum < 0 && sum < nums[i]) sum = 0;
+            sum += nums[i];
+            best = max(sum, best);
+        }
+        return best;
+    }
+};
