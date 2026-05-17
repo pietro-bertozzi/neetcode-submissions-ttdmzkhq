ecdfdddd7534fd91df2f195791f6e761f82b2689
Pietro Bertozzi <pietro.bertozzi.work@gmail.com>
2026-05-17 10:52:33 +0200

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+        for (int n : nums) {
+            if (seen.find(n) != seen.end()) return n;
+            seen.insert(n);
+        }
+    }
+};
